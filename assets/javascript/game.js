ec59805831e4a7f83377695b5ea5e3d3af9ef9a6
Nancy Gorman Lukas <nlukas13@optonline.net>
2016-10-23 15:17:14 -0400

    styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,179 @@
+window.onload = function() {
+	
+	var userGuess;
+	var words = ["jen", "rangy", "arnold", "nick", "julian", "stephen", "josie", "cristal", "amanda", "whitley", "karina", "travis", "frank", "todd", "yana", "kristin", "seth", "tom", "eli", "richard", "chen", "steven", "joshua", "tyler", "nancy", "sal", "hira"];
+	var word = [];
+	var positions = [];
+	var guessedLetters = [];
+	var guesses;
+	var wins;
+	var losses;
+	var count = 0;
+
+	// inital score
+	wins = 0;
+	losses = 0;
+	document.querySelector(".wins").innerHTML = wins;
+	document.querySelector(".losses").innerHTML = losses;
+
+	var num = 0;
+	newWord(num);
+
+
+	function selectNewWord() {
+		num = Math.floor((Math.random() * 80) + 1);
+		newWord(num);
+		console.log(words[num]);
+	}
+
+	
+
+	function newWord(num) {
+		// set guesses
+		document.querySelector(".info").innerHTML = "";
+		guesses = 6;
+		document.querySelector(".guesses").innerHTML = guesses;
+		// hangman picture
+		document.querySelector(".hangman").innerHTML = '<img src="assets/images/hang' + guesses + '.png">';
+		// clear positions and guesses
+		positions = [];
+		guessedLetters = [];
+		document.querySelector(".lettersGuessed").innerHTML = guessedLetters.join(" ");
+		// set positions of intial word and display
+		word = words[num].split("");
+		for (var i=0; i<word.length; i++) {
+			positions.push("_");
+		}
+		document.querySelector(".positions").innerHTML = positions.join(" ");
+
+		document.onkeyup = function(event) {
+			// reset  and already-guessed
+			document.querySelector(".info").innerHTML = "";
+			// get letter only
+			if (event.keyCode >= 65 && event.keyCode <= 90) {
+				userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+				checkGuessed(userGuess);
+			} else {
+				document.querySelector(".info").innerHTML = "please press a letter";
+			}
+
+			checkWin();
+		}
+	}
+
+	
+
+	function checkGuessed(userGuess) {
+		// see if letter matches any letters in word
+
+
+		check: {
+			for (var i=0; i<guessedLetters.length; i++) {
+				if (guessedLetters[i] == userGuess) {
+					document.querySelector(".info").innerHTML = "guess another letter";
+					break check;
+				}
+			}
+			for (var i=0; i<positions.length; i++) {
+				if (positions[i] == userGuess) {
+					document.querySelector(".info").innerHTML = "guess another letter";
+					break check;
+				}
+			}
+			replacements(userGuess);
+		}
+	}
+
+
+
+	function replacements(userGuess) {
+		// see if letter matches any letters in word
+		var replacements = 0;
+		for (var i=0; i<word.length; i++) {
+			if (word[i] == userGuess) {
+				positions[i] = word[i];
+				replacements++;
+			}
+		}
+		if (replacements == 0) {
+			//  no replacements reduce guesses
+			guesses--;
+			document.querySelector(".guesses").innerHTML = guesses;
+			document.querySelector(".hangman").innerHTML = '<img src="assets/images/hang' + guesses + '.png">';
+			// display guessed letters
+			guessedLetters.push(userGuess);
+			document.querySelector(".lettersGuessed").innerHTML = guessedLetters.join(" ");
+		} else {
+			// redisplay positions
+			document.querySelector(".positions").innerHTML = positions.join(" ");
+		}
+		checkWin();
+	}
+
+
+
+
+	function checkWin() {
+		// if all underscores are replaced
+		winning: {
+			for (var i=0; i<positions.length; i++) {
+				if (positions[i] == "_") {
+					break winning;
+				}
+			}
+			// display wins
+			wins++;
+			answerReset();
+			document.querySelector(".wins").innerHTML = wins;
+			document.querySelector(".hangman").innerHTML = '<img src="assets/images/youwon.png">';	
+		}
+		// guess reaches 0
+		if (guesses == 0) {
+			losses++;
+			document.querySelector(".losses").innerHTML = losses;
+			document.querySelector(".hangman").innerHTML = '<img src="assets/images/youlost.png">';
+			document.querySelector(".response").innerHTML = "the word was: " + words[num] + "<br> press enter to continue";
+			answerReset();
+		}
+	
+	}
+
+
+
+
+
+
+	function answerReset() {
+		// reset answer
+		//document.querySelector(".response").innerHTML = "";
+		document.querySelector(".anykey").innerHTML = "";
+		document.querySelector(".info").innerHTML = "";
+		selectNewWord();
+	}
+
+	function answer() {
+
+		// key to reset
+		document.onkeyup = function(event) {
+			answerReset();
+		}
+
+		document.querySelector(".guessLetter").addEventListener("click", function(){
+				
+				answerReset();
+
+		    
+		});
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
